35e9d9a3f5c884cc268cd1d2023d0921798701f4
北幽 <123240177+ser580579992138@users.noreply.github.com>
2025-08-21 02:06:13 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -45,8 +45,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			name: "青空图仓",
+			url: "https://v.520737.xyz", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
